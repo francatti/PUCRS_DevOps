55596b9a07fff5283fc83738e4769a6159ed176f
francatti <francatti.nicholas@gmail.com>
2025-05-28 20:02:16 -0300

    Configuração inicial do projeto de DevOps com Node.js e HelloWorld em HTML

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rota principal que serve o index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
