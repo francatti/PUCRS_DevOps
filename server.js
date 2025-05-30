903bf1457c51a769a480a584f91014967f55d9d2
francatti <francatti.nicholas@gmail.com>
2025-05-29 21:08:49 -0300

    Trocando a porta do server.js para 80 para conexão com o EC2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // Serve arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, "public")));
